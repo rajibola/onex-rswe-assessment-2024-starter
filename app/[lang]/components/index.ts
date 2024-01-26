5505415b7b5f3a8c4e0398800abcf0dc5947490e
Ridwan Ajibola <51078424+rajibola@users.noreply.github.com>
2024-01-26 21:42:29 +0100

    chore: remove file

--- app/[lang]/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { CustomCarousel } from "@/components/CustomCarousel";
-export { TableWithHeader } from "@/components/TableWithHeader";
-export { Sidebar } from "@/components/sidebar";
-export { PageHeader } from "@/components/page-header";
